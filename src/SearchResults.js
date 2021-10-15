1ec313454d4d8b6f19c97f726e8d6fb1eeacdc1b
andrejskalnins <andrejs.kalnins@gmail.com>
2021-10-15 21:14:30 +0100

    Lesson 2 Step 15 completed

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,14 +1,24 @@
 import React from "react";
 import SearchResultsHeader from "./SearchResultsHeader";
 import moment from "moment";
+import { useState } from "react";
+
 function SearchResults(props) {
+  const [rowSelected, setRowSelected] = useState(null);
+  function handleClick(index) {
+    setRowSelected(index);
+  }
   return (
     <table className="table table-responsive text-left">
       <SearchResultsHeader />
       <tbody>
         {props.results.map((customer, index) => {
           return (
-            <tr key={index}>
+            <tr
+              key={index}
+              onClick={() => handleClick(index)}
+              className={index === rowSelected ? "bg-success" : ""}
+            >
               <th scope="col">{customer.id}</th>
               <th scope="col">{customer.title}</th>
               <th scope="col">{customer.firstName}</th>
